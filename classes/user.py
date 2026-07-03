c6ac7b02047955815eeb2a7ac5d7e2d97f224c85
The Ghostwriter 25 <jeremiensenda10@gmail.com>
2026-07-03 10:51:47 +0200

    Update user.py

--- classes/user.py
+++ classes/user.py
@@ -5,8 +5,35 @@ from bcrypt import hashpw, gensalt, checkpw
 class User(DatabaseManager):
     def __init__(self):
         super().__init__()
+
+
+    def add_user(self, name:str, passwd:str):
+        passwd = hashpw(passwd)
         pass
 
+
+    def change_username(self, name:str, new_name:str, passwd:str):
+        pass
+
+
+    def change_password(self, name:str, passwd:str, naw_passwd:str):
+        pass
+    
+
+    def get_user_id(self, name:str):
+        pass
+
+
+    def get_users(self):
+        pass
+
+
+    def delete_user(self, ident:int, name:str, passwd:str):
+        pass
+
+
+    def check(self, name:str, passwd:str):
+        pass
 
 
     if __name__ == "__main__":
