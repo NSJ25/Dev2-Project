8096aa48c574906f390602a8cec7deae9b86de1a
The Ghostwriter 25 <jeremiensenda10@gmail.com>
2026-07-03 09:10:54 +0200

    Database_v4

---
+++ classes/answer.py
@@ -0,0 +1,13 @@
+from classes.database_manager import DatabaseManager
+
+
+class Answer(DatabaseManager):
+    def __init__(self):
+        super.__init__()
+        pass
+
+
+
+
+    if __name__ == "__main__":
+        pass
--- classes/status.py
+++ classes/status.py
@@ -43,5 +43,6 @@ class Status(DatabaseManager):
         else:
             return -1
 
+
     if __name__ == "__main__":
         pass
