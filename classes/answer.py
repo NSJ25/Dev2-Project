f3cb14980809a27532f124c577c9c09c8ac19bb3
The Ghostwriter 25 <jeremiensenda10@gmail.com>
2026-07-03 10:14:05 +0200

    Update answer.py

--- classes/answer.py
+++ classes/answer.py
@@ -3,7 +3,7 @@ from classes.database_manager import DatabaseManager
 
 class Answer(DatabaseManager):
     def __init__(self):
-        super.__init__()
+        super().__init__()
         pass
 
 
