2cbac8c43f0abfe6ab27683006d4746187b25413
The Ghostwriter 25 <jeremiensenda10@gmail.com>
2026-07-03 18:06:29 +0200

    implementation de la fonction get_games_by_date()

--- classes/game.py
+++ classes/game.py
@@ -8,24 +8,32 @@ class Game(DatabaseManager):
     def save_game(self, user_id,  score ):
         date_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.execute(
-            "INSERT INTO Games (user_id, date_game, score) VALUES (?, ?, ?) ",
-            (user_id, date_now, score)
+        "INSERT INTO Games (user_id, date_game, score) VALUES (?, ?, ?) ",
+    (user_id, date_now, score)
         )
         self.commit()
 
-    def get_games_by_user(self, user_id):
+    def get_games_by_user(self, name):
         self.execute(
     """SELECT U.username, G.score, G.date_game 
             FROM Games AS G JOIN Users AS U ON G.user_id = U.id
-            WHERE G.user_id = ?""",
-    (user_id,)
+            WHERE U.username = ? 
+            ORDER BY G.date_game DESC""",
+    (name,)
         )
         return self.fetchall()
 
 
     def get_games_by_date(self, date_game):
-        pass
-
+        date = datetime.strptime( date_game, "%d/%m/%Y").strftime("%Y-%m-%d")
+        self.execute(
+    """SELECT U.username, G.score, G.date_game 
+            FROM Games AS G JOIN Users AS U ON G.user_id = U.id
+            WHERE DATE(G.date_game) = ? 
+            ORDER BY G.date_game DESC""",
+    (date,)
+        )
+        return self.fetchall()
 
 
     if __name__ == "__main__":
