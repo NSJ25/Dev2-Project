f349a696541717b9a8a36fadee559bc7a05ec68e
The Ghostwriter 25 <jeremiensenda10@gmail.com>
2026-07-03 13:33:35 +0200

    implementation des fonctions add_user() & check()

--- classes/user.py
+++ classes/user.py
@@ -7,18 +7,23 @@ class User(DatabaseManager):
         super().__init__()
 
 
-    def add_user(self, name:str, passwd:str):
-        passwd = hashpw(passwd)
+    def add_user(self, name:str, password:str):
+        passwd = hashpw(password.encode("utf-8"), gensalt()).decode("utf-8")
+        self.execute(
+            "INSERT INTO Users (username, password) VALUES (?, ?)",
+            (name, passwd)
+        )
+        self.commit()
+
+
+
+    def change_username(self, name:str, new_name:str, password:str):
         pass
 
 
-    def change_username(self, name:str, new_name:str, passwd:str):
+    def change_password(self, name:str, password:str, naw_password:str):
         pass
 
-
-    def change_password(self, name:str, passwd:str, naw_passwd:str):
-        pass
-    
 
     def get_user_id(self, name:str):
         pass
@@ -28,17 +33,21 @@ class User(DatabaseManager):
         pass
 
 
-    def delete_user(self, ident:int, name:str, passwd:str):
+    def delete_user(self, ident:int, name:str, password:str):
         pass
 
 
-    def check(self, name:str, passwd:str):
-        pass
+    def check(self,name:str, password:str):
+        self.execute(
+            "SELECT password FROM Users WHERE username = ?",
+            (name,)
+        )
+        result = self.fetchone()
+        if result is not None:
+            return checkpw(password.encode("utf-8"), result[0].encode("utf-8"))
+        else:
+            return False
 
 
     if __name__ == "__main__":
         pass
-
-
-
-
