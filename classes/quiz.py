7a9a4fc26a4320f0efefc402bcbf881150a51b02
The Ghostwriter 25 <jeremiensenda10@gmail.com>
2026-07-03 21:11:49 +0200

    implementation de la la class Quiz

---
+++ classes/quiz.py
@@ -0,0 +1,19 @@
+from classes.subject import Subject
+from classes.game import Game
+from classes.user import User
+from classes.answer import Answer
+from classes.question import Question
+from classes.status import Status
+from random import random
+
+subject = Subject()
+game = Game()
+user = User()
+answer = Answer()
+question = Question()
+status = Status()
+
+
+class Quiz:
+    def __init__(self):
+        pass
